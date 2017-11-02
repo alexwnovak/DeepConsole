5af672bc7ae076b9c64cfb6a8d2f2c15440155d8
Alex Novak <alexwnovak@gmail.com>
2017-11-02 15:08:56 -0400

    +The arguments

--- src/DeepConsole/DeepConsole/SetConsoleColor.cs
+++ src/DeepConsole/DeepConsole/SetConsoleColor.cs
@@ -1,9 +1,36 @@
-﻿using System.Management.Automation;
+﻿using System;
+using System.Drawing;
+using System.Management.Automation;
 
 namespace DeepConsole
 {
    [Cmdlet( VerbsCommon.Set, "ConsoleColor" )]
    public class SetConsoleColor : PSCmdlet
    {
+      [Parameter(
+         Mandatory = true,
+         Position = 0,
+         HelpMessage = "The color index (from 0-15) for the color index to set"
+      )]
+      public int Index
+      {
+         get;
+         set;
+      }
+
+      [Parameter(
+         Mandatory = true,
+         Position = 1,
+         HelpMessage = "The comma-separated RGB values enclosed in double quotes"
+      )]
+      public Color Color
+      {
+         get;
+         set;
+      }
+
+      protected override void ProcessRecord()
+      {
+      }
    }
 }
