6324af6c8557a31f39349df0ea52a146a1e3a84c
Alex Novak <alexwnovak@gmail.com>
2017-11-02 15:17:50 -0400

    +SMALL_RECT

---
+++ src/DeepConsole/DeepConsole/Native/SMALL_RECT.cs
@@ -0,0 +1,10 @@
+﻿namespace DeepConsole.Native
+{
+   public struct SMALL_RECT
+   {
+      public short Left;
+      public short Top;
+      public short Right;
+      public short Bottom;
+   }
+}
