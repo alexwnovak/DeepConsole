8a061de583cf060b93d61829703847d28bff6235
Alex Novak <alexwnovak@gmail.com>
2017-11-02 15:22:39 -0400

    +NativeMethods internal static class for interop'd stuff

---
+++ src/DeepConsole/DeepConsole/Native/NativeMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DeepConsole.Native
+{
+   internal static class NativeMethods
+   {
+      public const int STD_OUTPUT_HANDLE = -11;
+
+      [DllImport( "kernel32.dll", SetLastError = true )]
+      public static extern IntPtr GetStdHandle( int nStdHandle );
+   }
+}
