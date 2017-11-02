9d22e5bcd4c3798e58688429ba6cc17c0936f0f3
Alex Novak <alexwnovak@gmail.com>
2017-11-02 15:15:33 -0400

    +COORD struct for interop

---
+++ src/DeepConsole/DeepConsole/Native/COORD.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DeepConsole.Native
+{
+   [StructLayout( LayoutKind.Sequential )]
+   public struct COORD
+   {
+      public short X;
+      public short Y;
+   }
+}
