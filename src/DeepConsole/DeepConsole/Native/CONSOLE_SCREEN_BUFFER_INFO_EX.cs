47e3e938dc0ec09f658321d1028015a7591d6bb3
Alex Novak <alexwnovak@gmail.com>
2017-11-02 15:18:31 -0400

    +CONSOLE_SCREEN_BUFFER_INFO_EX for grabbing lots of console info at once

---
+++ src/DeepConsole/DeepConsole/Native/CONSOLE_SCREEN_BUFFER_INFO_EX.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DeepConsole.Native
+{
+   [StructLayout( LayoutKind.Sequential )]
+   public struct CONSOLE_SCREEN_BUFFER_INFO_EX
+   {
+      public int cbSize;
+      public COORD dwSize;
+      public COORD dwCursorPosition;
+      public ushort wAttributes;
+      public SMALL_RECT srWindow;
+      public COORD dwMaximumWindowSize;
+      public ushort wPopupAttributes;
+      public bool bFullscreenSupported;
+      public COLORREF black;
+      public COLORREF darkBlue;
+      public COLORREF darkGreen;
+      public COLORREF darkCyan;
+      public COLORREF darkRed;
+      public COLORREF darkMagenta;
+      public COLORREF darkYellow;
+      public COLORREF gray;
+      public COLORREF darkGray;
+      public COLORREF blue;
+      public COLORREF green;
+      public COLORREF cyan;
+      public COLORREF red;
+      public COLORREF magenta;
+      public COLORREF yellow;
+      public COLORREF white;
+   }
+}
