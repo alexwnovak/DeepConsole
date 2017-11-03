aa2cd2dc8060ae5841b6a7426ac4387e4571600b
Alex Novak <alexwnovak@gmail.com>
2017-11-03 15:58:04 -0400

    +Cmdlet for getting one color

---
+++ src/DeepConsole/DeepConsole/Cmdlets/GetConsoleColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Management.Automation;
+using DeepConsole.Core;
+
+namespace DeepConsole.Cmdlets
+{
+   [Cmdlet( VerbsCommon.Get, "ConsoleColor" )]
+   public class GetConsoleColor : PSCmdlet
+   {
+      [Parameter(
+         Mandatory = true,
+         Position = 0,
+         HelpMessage = "The color index (from 0-15) for the color index to set"
+      )]
+      public int Index
+      {
+         get;
+         set;
+      }
+
+      protected override void ProcessRecord()
+      {
+         var consoleModifier = new ConsoleModifier();
+
+         var color = consoleModifier.GetColor( Index );
+
+         WriteObject( color );
+      }
+   }
+}
