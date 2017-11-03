02346c6056d70203bcb1c758b6cbaa6194e818bc
Alex Novak <alexwnovak@gmail.com>
2017-11-03 15:49:42 -0400

    +ConsoleController, a layer that sits below the cmdlet

---
+++ src/DeepConsole/DeepConsole/Controllers/ConsoleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using DeepConsole.Core;
+
+namespace DeepConsole.Controllers
+{
+   public class ConsoleController
+   {
+      private readonly IConsoleModifier _consoleModifier;
+
+      public ConsoleController( IConsoleModifier consoleModifier )
+      {
+         _consoleModifier = consoleModifier;
+      }
+
+      public void SetColor( int index, Color color )
+      {
+         _consoleModifier.SetColor( index, color );
+      }
+   }
+}
