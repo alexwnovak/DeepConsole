0862a57168a581c600afe7d9e7ecc5d0f9ed72d7
Alex Novak <alexwnovak@gmail.com>
2017-11-03 19:47:03 -0400

    +Basic feature file for SetConsoleColor cmdlet

---
+++ src/DeepConsole/DeepConsole.AcceptanceTests/Steps/SetConsoleColorFeatureSteps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using TechTalk.SpecFlow;
+
+namespace DeepConsole.AcceptanceTests.Steps
+{
+   [Binding]
+   public class SetConsoleColorFeatureSteps
+   {
+      [StepArgumentTransformation]
+      public Color ColorTransform( string colorString )
+      {
+         var colorConverter = new ColorConverter();
+         return (Color) colorConverter.ConvertFromString( colorString );
+      }
+
+      [When( @"I call SetConsoleColor with index (.*) and color (.*)" )]
+      public void WhenICallSetConsoleColorWithIndexAndColorRed( int index, Color color )
+      {
+         ScenarioContext.Current.Pending();
+      }
+
+      [Then( @"console color (.*) should be (.*)" )]
+      public void ThenConsoleColorShouldBeRed( int index, Color color )
+      {
+         ScenarioContext.Current.Pending();
+      }
+   }
+}
