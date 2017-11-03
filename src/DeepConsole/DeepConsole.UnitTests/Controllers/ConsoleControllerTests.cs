7e7a0b6d40e427b68724269032db23c385414f53
Alex Novak <alexwnovak@gmail.com>
2017-11-03 19:24:07 -0400

    +Test for SetColor

--- src/DeepConsole/DeepConsole.UnitTests/Controllers/ConsoleControllerTests.cs
+++ src/DeepConsole/DeepConsole.UnitTests/Controllers/ConsoleControllerTests.cs
@@ -1,6 +1,29 @@
-﻿namespace DeepConsole.UnitTests.Controllers
+﻿using System.Drawing;
+using DeepConsole.Controllers;
+using DeepConsole.Core;
+using Xunit;
+using NanoBuilder;
+using Moq;
+
+namespace DeepConsole.UnitTests.Controllers
 {
    public class ConsoleControllerTests
    {
+      [Fact]
+      public void SetColor_SettingColor_ControllerUsesConsoleModifier()
+      {
+         const int index = 123;
+         var color = Color.Red;
+
+         var consoleModifierMock = new Mock<IConsoleModifier>();
+
+         var controller = ObjectBuilder.For<ConsoleController>()
+            .With( consoleModifierMock.Object )
+            .Build();
+
+         controller.SetColor( index, color );
+
+         consoleModifierMock.Verify( cm => cm.SetColor( index, color ), Times.Once() );
+      }
    }
 }
