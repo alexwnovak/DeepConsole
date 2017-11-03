ffb3e9d34a9e019e45d6d322305f18290ae90dcc
Alex Novak <alexwnovak@gmail.com>
2017-11-03 15:57:51 -0400

    +GetColor method for resolving a single color

--- src/DeepConsole/DeepConsole.Core/ConsoleModifier.cs
+++ src/DeepConsole/DeepConsole.Core/ConsoleModifier.cs
@@ -6,6 +6,19 @@ namespace DeepConsole.Core
 {
    public class ConsoleModifier : IConsoleModifier
    {
+      public Color GetColor( int index )
+      {
+         var stdout = NativeMethods.GetStdHandle( NativeMethods.STD_OUTPUT_HANDLE );
+
+         var bufferInfo = new CONSOLE_SCREEN_BUFFER_INFO_EX();
+         bufferInfo.cbSize = Marshal.SizeOf( bufferInfo );
+
+         NativeMethods.GetConsoleScreenBufferInfoEx( stdout, ref bufferInfo );
+
+         var tableColor = bufferInfo.ColorTable[index];
+         return Color.FromArgb( tableColor.R, tableColor.G, tableColor.B );
+      }
+
       public void SetColor( int index, Color color )
       {
          var stdout = NativeMethods.GetStdHandle( NativeMethods.STD_OUTPUT_HANDLE );
--- src/DeepConsole/DeepConsole.Core/IConsoleModifier.cs
+++ src/DeepConsole/DeepConsole.Core/IConsoleModifier.cs
@@ -4,6 +4,7 @@ namespace DeepConsole.Core
 {
    public interface IConsoleModifier
    {
+      Color GetColor( int index );
       void SetColor( int index, Color color );
    }
 }
