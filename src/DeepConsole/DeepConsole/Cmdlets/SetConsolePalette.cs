b055e8a56308a788a5ccb949a43b69f8d05ffb8a
Alex Novak <alexwnovak@gmail.com>
2017-11-07 20:50:57 -0500

    +SetConsolePalette cmdlet stub
    This one will set multiple colors based on a JSON file.

---
+++ src/DeepConsole/DeepConsole/Cmdlets/SetConsolePalette.cs
@@ -0,0 +1,9 @@
+﻿using System.Management.Automation;
+
+namespace DeepConsole.Cmdlets
+{
+   [Cmdlet( VerbsCommon.Set, "ConsolePalette" )]
+   public class SetConsolePalette : PSCmdlet
+   {
+   }
+}
