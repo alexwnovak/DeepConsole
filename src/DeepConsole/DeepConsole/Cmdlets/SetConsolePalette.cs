353361c95036bfd9d42e10e0e1a2762a7b05883c
Alex Novak <alexwnovak@gmail.com>
2017-11-07 20:57:11 -0500

    Scaffolding for SetConsolePalette handing it to the controller

--- src/DeepConsole/DeepConsole/Cmdlets/SetConsolePalette.cs
+++ src/DeepConsole/DeepConsole/Cmdlets/SetConsolePalette.cs
@@ -1,9 +1,27 @@
 ﻿using System.Management.Automation;
+using DeepConsole.Controllers;
+using DeepConsole.Core;
 
 namespace DeepConsole.Cmdlets
 {
    [Cmdlet( VerbsCommon.Set, "ConsolePalette" )]
    public class SetConsolePalette : PSCmdlet
    {
+      [Parameter(
+         Mandatory = true,
+         Position = 0,
+         HelpMessage = "The JSON file that describes the color palette"
+      )]
+      public string PaletteFilePath
+      {
+         get;
+         set;
+      }
+
+      protected override void ProcessRecord()
+      {
+         var controller = new ConsoleController( new ConsoleModifier() );
+         controller.SetColorPalette( PaletteFilePath );
+      }
    }
 }
