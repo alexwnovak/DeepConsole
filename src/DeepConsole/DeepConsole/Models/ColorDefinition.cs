420ff4d911bc686702a9231e8e303826cbf57701
Alex Novak <alexwnovak@gmail.com>
2017-11-07 21:26:18 -0500

    +ColorDefinition and ColorPalette
    A way to carry around single colors and an aggregate of them.

---
+++ src/DeepConsole/DeepConsole/Models/ColorDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace DeepConsole.Models
+{
+   public struct ColorDefinition
+   {
+      public int Index
+      {
+         get;
+      }
+
+      public Color Color
+      {
+         get;
+      }
+
+      public ColorDefinition( int index, Color color )
+      {
+         Index = index;
+         Color = color;
+      }
+   }
+}
---
+++ src/DeepConsole/DeepConsole/Models/ColorPalette.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DeepConsole.Models
+{
+   public class ColorPalette
+   {
+      private readonly List<ColorDefinition> _colorDefinitions = new List<ColorDefinition>();
+      public IReadOnlyCollection<ColorDefinition> ColorDefinitions => _colorDefinitions.AsReadOnly();
+
+      public void Add( ColorDefinition colorDefinition ) => _colorDefinitions.Add( colorDefinition );
+   }
+}
