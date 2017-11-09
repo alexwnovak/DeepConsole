1eaebea74797dee229eccd89f47879fae20f318a
Alex Novak <alexwnovak@gmail.com>
2017-11-08 21:40:13 -0500

    Moving Json parsing into its own class specialized for palettes
    Also turns strings into Json, not FILES into Json, breaking up the dependencies even further.

--- src/DeepConsole/DeepConsole.UnitTests/Models/ColorPaletteReaderTests.cs
+++ src/DeepConsole/DeepConsole.UnitTests/Models/ColorPaletteReaderTests.cs
@@ -1,6 +1,44 @@
-﻿namespace DeepConsole.UnitTests.Models
+﻿using System.Drawing;
+using Xunit;
+using FluentAssertions;
+using DeepConsole.Models;
+
+namespace DeepConsole.UnitTests.Models
 {
    public class ColorPaletteReaderTests
    {
+      [Fact]
+      public void FromJson_ContainsOneColorDefinition_DefinitionIsRead()
+      {
+         const string json = "{ \"colors\": [ { index: 7, color: \"0,255,0\" } ] }";
+
+         var colorPaletteReader = new ColorPaletteReader();
+
+         var colorPalette = colorPaletteReader.FromJson( json );
+
+         colorPalette.ColorDefinitions
+            .Should()
+            .HaveCount( 1 )
+            .And
+            .Contain( cd => cd.Index == 7 && cd.Color == Color.Lime );
+      }
+
+      [Fact]
+      public void FromJson_ContainsTwoColorDefinitions_DefinitionsAreRead()
+      {
+         const string json = "{ \"colors\": [ { index: 0, color: \"0,0,0\" }, { index: 7, color: \"255,0,0\" } ] }";
+
+         var colorPaletteReader = new ColorPaletteReader();
+
+         var colorPalette = colorPaletteReader.FromJson( json );
+
+         colorPalette.ColorDefinitions
+            .Should()
+            .HaveCount( 2 )
+            .And
+            .Contain( cd => cd.Index == 0 && cd.Color == Color.Black )
+            .And
+            .Contain( cd => cd.Index == 7 && cd.Color == Color.Red );
+      }
    }
 }
--- src/DeepConsole/DeepConsole/Models/ColorPaletteReader.cs
+++ src/DeepConsole/DeepConsole/Models/ColorPaletteReader.cs
@@ -1,6 +1,29 @@
-﻿namespace DeepConsole.Models
+﻿using System.Drawing;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace DeepConsole.Models
 {
    public class ColorPaletteReader
    {
+      public ColorPalette FromJson( string json )
+      {
+         JObject jsonObject = JObject.Parse( json );
+
+         var colorConverter = new ColorConverter();
+
+         var indices = jsonObject["colors"].Select( c => (int) c["index"] ).ToArray();
+         var colors = jsonObject["colors"].Select( c => (Color) colorConverter.ConvertFromString( (string) c["color"] ) ).ToArray();
+
+         var colorPalette = new ColorPalette();
+
+         for ( int index = 0; index < indices.Length; index++ )
+         {
+            var colorDefinition = new ColorDefinition( indices[index], colors[index] );
+            colorPalette.Add( colorDefinition );
+         }
+
+         return colorPalette;
+      }
    }
 }
